11cf8b00e46cc33b9ba99af7fafdceda4420ec2c
llyas-36 <sonofhis18@gmail.com>
2025-10-12 00:48:07 +0300

    modify main.go

--- main.go
+++ main.go
@@ -1,138 +1,113 @@
- package main
+// package main
 
 // import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"io"
-// 	"log"
-// 	"net/http"
+//     "encoding/json"
+//     "fmt"
+//     "io"
+//     "log"
+//     "net/http"
 // )
 
-<<<<<<< HEAD
-func handle(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-<<<<<<< HEAD
-	username := ""
-=======
-
->>>>>>> ea598b5 (Remove WakaTime API key)
-	password := ""
-	credentials := fmt.Sprintf("%s:%s", username, password)
-	//	encodedCredentials := base64.StdEncoding.EncodeToString([]byte(credentials))
-=======
 // func handle(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
+//     w.Header().Set("Content-Type", "application/json")
+//     username := ""
+//     password := ""
+//     credentials := fmt.Sprintf("%s:%s", username, password)
+//     // encodedCredentials := base64.StdEncoding.EncodeToString([]byte(credentials))
 
-// 	password := ""
-// 	credentials := fmt.Sprintf("%s:%s", username, password)
-// 	//	encodedCredentials := base64.StdEncoding.EncodeToString([]byte(credentials))
->>>>>>> 7c051b8 (update main.go)
+//     url := "https://wakatime.com/api/v1/users/current/stats/last_7_days"
 
-// 	url := "https://wakatime.com/api/v1/users/current/stats/last_7_days"
+//     req, err := http.NewRequest("GET", url, nil)
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+//     req.Header.Set("Authorization", "Basic "+credentials)
 
-// 	req, err := http.NewRequest("GET", url, nil)
-// 	if err != nil {
-// 		log.Fatal(err)
+//     client := http.Client{}
+//     res, err := client.Do(req)
+//     if err != nil {
+//         log.Fatal(err)
+//     }
 
-// 	}
-// 	req.Header.Set("Authorization", "Basic "+credentials)
+//     body, err := io.ReadAll(res.Body)
+//     if err != nil {
+//         log.Fatal(err)
+//     }
 
-// 	client := http.Client{}
-// 	res, err := client.Do(req)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	body, err := io.ReadAll(res.Body)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	fmt.Fprintf(w, string(body))
-
+//     fmt.Fprintf(w, string(body))
 // }
 
 // func Day_7(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	url := "https://wakatime.com/api/v1/users/current/stats/last_7_days"
-// 	auth := fmt.Sprintf("%s:%s", "", "")
+//     w.Header().Set("Content-Type", "application/json")
+//     url := "https://wakatime.com/api/v1/users/current/stats/last_7_days"
+//     auth := fmt.Sprintf("%s:%s", "", "")
 
-// 	req, err := http.NewRequest("GET", url, nil)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+//     req, err := http.NewRequest("GET", url, nil)
+//     if err != nil {
+//         panic(err)
+//     }
 
-// 	req.Header.Set("Authorization", "Basic "+auth)
+//     req.Header.Set("Authorization", "Basic "+auth)
 
-// 	client := http.DefaultClient
-// 	res, err := client.Do(req)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	body, err := io.ReadAll(res.Body)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer res.Body.Close()
-// 	var data map[string]any
-// 	err = json.Unmarshal(body, &data)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	assertData := data["data"].(map[string]interface{})
-// 	//categories := assertData["categories"].([]interface{})
-// 	//languages := assertData["languages"].([]interface{})
-// 	projects := assertData["projects"].([]interface{})
-// 	jsonDD, err := json.Marshal(projects)
+//     client := http.DefaultClient
+//     res, err := client.Do(req)
+//     if err != nil {
+//         panic(err)
+//     }
+//     body, err := io.ReadAll(res.Body)
+//     if err != nil {
+//         panic(err)
+//     }
+//     defer res.Body.Close()
 
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	type Activity struct {
-// 		Name    string  `json:"name"`
-// 		Percent float64 `json:"percent"`
-// 		Text    string  `json:"text"`
-// 	}
-// 	var activity []Activity
-// 	jsonerr := json.Unmarshal([]byte(jsonDD), &activity)
-// 	if jsonerr != nil {
-// 		panic(jsonerr)
-// 	}
+//     var data map[string]any
+//     err = json.Unmarshal(body, &data)
+//     if err != nil {
+//         panic(err)
+//     }
 
-// 	for _, a := range activity {
-// 		fmt.Printf("%s → %s\nPercent → %v\n", a.Name, a.Text, a.Percent)
-// 	}
+//     assertData := data["data"].(map[string]interface{})
+//     projects := assertData["projects"].([]interface{})
+//     jsonDD, err := json.Marshal(projects)
+//     if err != nil {
+//         panic(err)
+//     }
 
-// 	container, _ := json.Marshal(activity)
+//     type Activity struct {
+//         Name    string  `json:"name"`
+//         Percent float64 `json:"percent"`
+//         Text    string  `json:"text"`
+//     }
 
-// 	fmt.Fprintf(w, string(container))
+//     var activity []Activity
+//     jsonerr := json.Unmarshal([]byte(jsonDD), &activity)
+//     if jsonerr != nil {
+//         panic(jsonerr)
+//     }
 
+//     for _, a := range activity {
+//         fmt.Printf("%s → %s\nPercent → %v\n", a.Name, a.Text, a.Percent)
+//     }
+
+//     container, _ := json.Marshal(activity)
+//     fmt.Fprintf(w, string(container))
 // }
 
-<<<<<<< HEAD
-func dailyAverage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	url := "https://wakatime.com/api/v1/users/current/stats/last_7_days"
-	auth := fmt.Sprintf("%s:%s", "", "")
-=======
 // func dailyAverage(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	url := "https://wakatime.com/api/v1/users/current/stats/last_7_days"
-// 	auth := fmt.Sprintf("%s:%s", "", "")
->>>>>>> 7c051b8 (update main.go)
+//     w.Header().Set("Content-Type", "application/json")
+//     url := "https://wakatime.com/api/v1/users/current/stats/last_7_days"
+//     auth := fmt.Sprintf("%s:%s", "", "")
 
-// 	req, err := http.NewRequest("GET", url, nil)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+//     req, err := http.NewRequest("GET", url, nil)
+//     if err != nil {
+//         panic(err)
+//     }
 
-// 	req.Header.Set("Authorization", "Basic "+auth)
+//     req.Header.Set("Authorization", "Basic "+auth)
 // }
+
 // func main() {
-
-// 	http.HandleFunc("/", handle)
-// 	fmt.Println("server running")
-
-// 	http.ListenAndServe(":8080", nil)
-
+//     http.HandleFunc("/", handle)
+//     fmt.Println("server running")
+//     http.ListenAndServe(":8080", nil)
 // }
